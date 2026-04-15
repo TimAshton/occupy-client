7b9ef6d28047e1bca6a1e60dda3c72003742f9ee
Tim Ashton <tim.ashton.la@gmail.com>
2026-04-15 16:06:13 -0700

    Add coverage thresholds

--- test/components/sector.test.js
+++
@@ -1,7 +0,0 @@
-import { assert, it, expect, describe } from 'vitest';
-
-import { Sector } from '../../src/components/Sector';
-
-describe('Sector component', () => {
-    it.skip('should render correctly', () => {});
-});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    test: {
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            thresholds: {
+                lines: 100,
+                functions: 100,
+                branches: 100,
+                statements: 100,
+            },
+        },
+    },
+});
