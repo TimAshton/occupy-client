b049ec629792b51775c4162e3798d82c1a75c0ba
Tim Ashton <tim.ashton.la@gmail.com>
2026-04-22 13:21:57 -0700

    Implement create game UI and API call

--- src/components/CreateGame.tsx
+++ src/components/CreateGame.tsx
@@ -2,13 +2,20 @@ import { useState } from 'react';
 
 export default function CreateGame() {
     const [data, setData] = useState([]);
+    const [inviteCode, setInviteCode] = useState();
 
     const createClickHandler = async () => {
         try {
             const response = await fetch(
-                'https://jsonplaceholder.typicode.com/todos/1',
+                'https://dxgduggtf0.execute-api.us-west-2.amazonaws.com/prod/game/create',
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ player_count: 1 }),
+                },
             );
             const result = await response.json();
+            setInviteCode(JSON.parse(result)['inviteCode']);
             setData(result);
         } catch (error) {
             console.error('Error', error);
@@ -19,7 +26,6 @@ export default function CreateGame() {
 
     return (
         <div className="my-15 flex flex-wrap items-center gap-6 p-4 w-full max-w-[420px] mx-auto">
-            {/* Player Selection (Sets the height of the row) */}
             <div className="flex flex-col gap-1">
                 <div className="text-sm font-medium">How many players?</div>
                 <select className="border rounded-lg p-1.5 bg-white">
@@ -27,10 +33,8 @@ export default function CreateGame() {
                     <option>2</option>
                 </select>
             </div>
-
-            {/* Action Button - Aligned to bottom */}
             <div className="flex flex-col self-stretch justify-end">
-                <button 
+                <button
                     className="bg-blue-600 text-white px-5 py-2 rounded-lg font-semibold hover:bg-blue-700 transition-colors"
                     onClick={() => createClickHandler()}
                 >
@@ -38,12 +42,12 @@ export default function CreateGame() {
                 </button>
             </div>
 
-            {/* Data Output */}
-            <div className="flex-1 min-w-[200px] bg-slate-50 p-3 rounded-lg border border-slate-200 self-stretch flex items-end">
-                <code className="text-xs break-all text-slate-600">
-                    {JSON.stringify(data)}
-                </code>
-            </div>
+            {true && (
+                <div className="flex-1 min-w-[200px] bg-slate-50 p-3 rounded-lg border border-slate-200 self-stretch flex items-end">
+                    <div>Invite Code:</div>
+                    <div>{inviteCode}</div>
+                </div>
+            )}
         </div>
     );
 }
